fa6887c77ed1c5f53264b3734854443d36fc893a
avimehenwal <avi.mehanwal@gmail.com>
2020-08-15 21:26:18 +0200

    feat(firebase-fn): :tada: Add hardcoded stripe backend

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # zinestore
 :shopping_cart: zinestore
+
+```
+firebase functions:config:set someservice.key="THE API KEY" someservice.id="THE CLIENT ID"
+firebase functions:config:get
+```
+
+#### :v: Get in touch with me
+
+> I am looking for Jobs ... :sunglasses:
+
+* [Github](https://github.com/avimehenwal/)
+* [My Website](https://avimehenwal.in)
+* [My Blog v2](https://avimehenwal2.netlify.app/)
+* [Twitter Handle](https://twitter.com/avimehenwal)
+* [LinkedIn](https://in.linkedin.com/in/avimehenwal)
+* [Stackoverflow](https://stackoverflow.com/users/1915935/avi-mehenwal)
+
+<a href="https://www.buymeacoffee.com/F1j07cV" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
+
+ Spread Love :hearts: and not :no_entry_sign: hatred   [![Twitter Follow](https://img.shields.io/twitter/follow/avimehenwal.svg?style=social)](https://twitter.com/avimehenwal)
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,116 @@
+require('dotenv').config()
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// globals
+const stripe = require('stripe')(process.env.stripekey)
+const cors = require('cors')({ origin: true });
+
+/*
+*  [x] Return json
+*  [x] deal with POST request
+*  [x] use full blown express app
+* Advantages
+* [ ] routing
+* [ ] middleware
+* [ ] cors
+* Disadvantages
+* [ ] Do not get the emulator console
+*/
+
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  const result = {
+    body: request.body,
+    query: request.query,
+    method: request.method,
+    rawBody: request.rawBody,
+    headers: request.get('User-Agent'),
+    data: '',
+  }
+
+  // RESTful http verb handler
+  switch (request.method) {
+    case 'GET': {
+      result.data = 'GET handler called'
+      break;
+    }
+    case 'POST': {
+      result['data'] = 'POST handler called'
+      break;
+    }
+    default: {
+      result['data'] = 'OTHER handler called'
+      break;
+    }
+  }
+
+  functions.logger.debug(result.query)
+  // Terminate redirect, end, send
+  response.status(200).json(result);
 });
+
+/*
+  Checkout Sessions expire 24 hours after creation.
+  deal with Promises - functions.https.onRequest( async (request, response) => {
+  add cors middleware to avoid
+  Access to XMLHttpRequest at 'http://---' from origin 'http://192.168.0.106:8080' has been blocked by CORS policy:
+  No 'Access-Control-Allow-Origin' header is present on the requested resource.
+*/
+export const stripebackend = functions.https.onRequest((request, response) => {
+  // when trying to use async, await, error
+  // Did you mean to mark this function as 'async'?
+  cors(request, response, () => {
+    stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          // currency: 'usd',
+          currency: 'EUR',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,        // in pennies
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'http://localhost:8080/success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'http://localhost:8080/failure',
+    }, function (err: Object, session: Object) {
+      response.send(session)
+    })
+  })
+})
+
+/*
+{
+  "id": "cs_test_TWZ1BYGkSyvKtzTzhCmMEckwoCeK4A0bLZfyP3FURyXLn3tN14xImSpI",
+  "object": "checkout.session",
+  "allow_promotion_codes": null,
+  "amount_subtotal": 2000,
+  "amount_total": 2000,
+  "billing_address_collection": null,
+  "cancel_url": "http://localhost:8080/failure",
+  "client_reference_id": null,
+  "currency": "eur",
+  "customer": null,
+  "customer_email": null,
+  "livemode": false,
+  "locale": null,
+  "metadata": {},
+  "mode": "payment",
+  "payment_intent": "pi_1HGUhND0sxgkGHoQvCwsSTVT",
+  "payment_method_types": [
+    "card"
+  ],
+  "setup_intent": null,
+  "shipping": null,
+  "shipping_address_collection": null,
+  "submit_type": null,
+  "subscription": null,
+  "success_url": "http://localhost:8080/success?session_id={CHECKOUT_SESSION_ID}",
+  "total_details": {
+    "amount_discount": 0,
+    "amount_tax": 0
+  }
+}
+*/
