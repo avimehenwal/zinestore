7b1959539e4ba36375098f73e06d8573b9b37a32
avimehenwal <avi.mehanwal@gmail.com>
2020-08-18 04:00:55 +0200

    feat(firebase-fn): :sunglasses: Get product data from DB
    * typescript able to read from json file by adding resolveJsonModule compiler option
    * Ignore TS7006 "noImplicitAny": false
    * expressjs add body-parser middleware to parse arguments in POST request.
    * 3 ways to test if an Object has a property

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,70 @@
 require('dotenv').config()
 import * as functions from 'firebase-functions';
+import os = require('os');
+import products from './database.json'
+import * as cors from 'cors'
+// const corsHandler = cors({ origin: true });
+// const cors = require('cors')({origin: true});
+// app.use(cors);
 
 // globals
 const stripe = require('stripe')(process.env.stripekey)
-const cors = require('cors')({ origin: true });
-const base = 'http://localhost:3000'
+let base = (os.hostname().match(/dynac/)) ? 'http://localhost:3000' : 'https://zinestore.netlify.app/'
+functions.logger.info('Using RootURL : ' + base)
+let result: object = {}
+
+
+function getProductById(id: string): object {
+  let product: object = products.find((item) => item.id === id)
+  if (product === undefined) {
+    // no match found
+    product = {
+      id: id,
+      msg: 'ID does not exist'
+    }
+  }
+  console.log(product)
+  return product
+}
+
+function getHandler(querystring: object): object {
+  result['handler'] = 'GET'
+  result['queryString'] = querystring
+  result['code'] = 200
+  return result
+}
+
+function postHandler(req) {
+  result['handler'] = 'POST'
+  if (req.hasOwnProperty('query')) {
+    result['queryString'] = req.query
+  }
+  if (req.hasOwnProperty('body')) {
+    result['arguments'] = req.body
+    if ('productId' in req.body) {
+      // get product details from DB
+      result['productData'] = getProductById(req.body.productId)
+      result['code'] = 200
+    } else {
+      result['error'] = {
+        msg: 'No productID found in body',
+        status: 'Bad Request',
+        desc: `server cannot or will not process the request due to something that
+        is perceived to be a client error (e.g., malformed request syntax, invalid request
+          message framing, or deceptive request routing).`
+      }
+      result['code'] = 400
+    }
+  }
+  return result
+}
+
+function otherHandler(rawBody): object {
+  result['handler'] = 'OTHERS'
+  result['arguments'] = rawBody
+  result['code'] = 400
+  return result
+}
 
 /*
 *  [x] Return json
@@ -49,6 +109,8 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.status(200).json(result);
 });
 
+
+
 /*
   Checkout Sessions expire 24 hours after creation.
   deal with Promises - functions.https.onRequest( async (request, response) => {
@@ -56,30 +118,63 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   Access to XMLHttpRequest at 'http://---' from origin 'http://192.168.0.106:8080' has been blocked by CORS policy:
   No 'Access-Control-Allow-Origin' header is present on the requested resource.
 */
-export const stripebackend = functions.https.onRequest((request, response) => {
+export const createPaymentIntent = functions.https.onRequest(async (request, response) => {
   // when trying to use async, await, error
   // Did you mean to mark this function as 'async'?
-  cors(request, response, () => {
-    stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        price_data: {
-          // currency: 'usd',
-          currency: 'EUR',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 1000,        // in pennies
+
+  /* Loose firebase logging! very bad */
+  // corsHandler(request, response, () => {})
+  /* Accept CORS */
+  response.set('Access-Control-Allow-Origin', '*');
+
+  /* Fresh Object, else reuses the old global object */
+  let res: object = {}
+
+  switch (request.method) {
+    case 'GET': {
+      res = getHandler(request.query)
+      break;
+    }
+    case 'POST': {
+      res = postHandler(request)
+      break;
+    }
+    default: {
+      res = otherHandler(request)
+      break;
+    }
+  }
+
+  const session: object = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        // currency: 'EUR',
+        product_data: {
+          name: res['productData'].name,
         },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: base + '/success?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: base + '/failure',
-    }, function (err: Object, session: Object) {
-      response.send(session)
-    })
+        // in pennies
+        unit_amount: Number(res['productData'].price),
+      },
+      quantity: Number(request.body.quantity),
+    }],
+    mode: 'payment',
+    success_url: base + '/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: base + '/failure',
   })
+
+  res['stripe'] = session
+  /* ISSUE
+    send a req with productID and then without productID => both will contain product info
+    and vice versa
+  */
+  functions.logger.warn(res['stripe'])
+  if (res.hasOwnProperty('error')) {
+    response.status(res['code']).json(res)
+  } else {
+    response.status(res['code']).json(res)
+  }
 })
 
 /*
@@ -115,3 +210,4 @@ export const stripebackend = functions.https.onRequest((request, response) => {
   }
 }
 */
+
---
+++ functions/src/test.ts
@@ -0,0 +1,18 @@
+// import products from './database.json'
+
+// function getProductById (id: string): object {
+//   let product: object = products.find( (item) => item.id === id )
+//   if (product === undefined) {
+//     // no match found
+//     product = {
+//       id: id,
+//       msg: 'ID does not exist'
+//     }
+//   }
+//   console.log(product)
+//   return product
+// }
+
+// getProductById('440e2eac-67ed-4407-993a-d9f6b7f15087')
+// getProductById('440e2eac-67ed-4')
+
